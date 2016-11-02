027bb6b3a916b1971716e2cb49b2804cfd2efc83
kjhark24 <kjhark24@gmail.com>
2016-11-02 17:01:35 -0400

    Text scraper final
    should be good to go

--- Textscraper/textScraper.py
+++ Textscraper/textScraper.py
@@ -4,17 +4,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import json
 
+#Create web browser to execute javascript so the menu can be loaded
 browserC = webdriver.Chrome("/home/kyle/Desktop/textScraper/chromedriver")   
 #specify the url
 commons = 'https://calvin.edu/directory/places/commons-dining-hall'
 knoll = 'https://calvin.edu/directory/places/knollcrest-dining-hall'
 
+#get commons webpage
 browserC.get(commons)
 WebDriverWait(browserC, 10).until(
     EC.visibility_of_element_located((By.ID, "myMenuContainer")))
+#save html of webpage after fully loading
 pageC = browserC.page_source
+#exit browser
 browserC.quit()
 
 browserK = webdriver.Chrome("/home/kyle/Desktop/textScraper/chromedriver") 
@@ -24,18 +27,15 @@ WebDriverWait(browserK, 10).until(
 pageK = browserK.page_source
 browserK.quit()
 
-#Query the website and return the html to the variable 'page'
-
-#import the Beautiful soup functions to parse the data returned from the website
-
-
 #Parse the html in the 'page' variable, and store it in Beautiful Soup format
-soupC = BeautifulSoup(pageC)
-soupK = BeautifulSoup(pageK)
+soupC = BeautifulSoup(pageC, "html5lib")
+soupK = BeautifulSoup(pageK, "html5lib")
 
+#find the menu in the html
 menuC = soupC.find('div', {'id':'myMenuContainer'})
 menuK = soupK.find('div', {'id':'myMenuContainer'})
 
+#format text to be useable and look nice
 menuC = menuC.get_text()
 menuC = menuC.replace("BreakfastHot", "Breakfast:\nHot")
 menuC = menuC.replace("LunchSoup", "\n\n\nLunch: \nSoup")
@@ -45,16 +45,33 @@ menuC = menuC.replace("Piazza", "\nPiazza")
 menuC = menuC.replace("UpperCrust", "\nUpperCrust")
 menuC = menuC.replace("DinnerSoup", "\n\n\nDinner: \nSoup")
 
+#format text to be useable and look nice, bad knollcrest edition 
 k = ''
 for content in menuK.contents:
-     k += str(content)
+     k += "\n" + str(content)
 
-print menuC
-print menuK
+k = k.replace("<p>", "")
+k = k.replace("</p>", "")
+k = k.replace("<strong>", "")
+k = k.replace("</strong>", "")
+k = k.replace("<h3>", "")
+k = k.replace("</h3>", ":")
+k = k.replace("<br/>", "\n")
+k = k.replace("<span style=\"text-decoration: underline;\">", "")
+k = k.replace("</span>", ":")
+k = k.replace("<hr/>", "")
+k = k.replace("\nVegan: Soup:\n", "Vegan Soup:")
 
+
+print menuC 
+print "\n\n" 
+print k
+
+#write menus to txt file for app use
 textC = open("commons.txt", "w")
 textC.write(menuC.encode('utf-8'))
 textC.close()
 
-
-
+textK = open("knoll.txt", "w")
+textK.write(k)
+textK.close()
