c16ccb568cc32fd2344c4a16c873532546bda080
Krisofer Brink <kpgbrink@gmail.com>
2016-11-07 00:01:03 -0500

    Server gets information from Calvin College Dining api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const express = require('express');
 const moment = require('moment-timezone');
 const cacheResponseDirective = require('express-cache-response-directive');
+const https = require('https');
 
 var app = express();
 
@@ -9,6 +12,25 @@ app.get('/', function (req, res) {
 });
 
 app.use(cacheResponseDirective());
+
+// send today over
+app.get('/venues', function (req, res) {
+    res.cacheControl({maxAge: "2h"});
+    res.send(venues);
+});
+
+module.exports = app;
+
+
+function Venue (name, events) {
+    this.name = name;
+    this.events = events = [];
+}
+
+Venue.prototype.addEvent = function (event) {
+    this.events.push(event);
+}
+
 
 function mealMenu (name, startTime, endTime, description) {
     this.name = name;
@@ -21,23 +43,77 @@ function mkeMmt (time) {
     return moment.tz(time, "hh:mm a", "America/Detroit");
 }
 
-var todaysMenu = [{"name": "Commons",
-    "events": [
-    new mealMenu('breakfast', mkeMmt('8:30 a'), mkeMmt('10:00 a'), 'contenental breakfast\nfdadffdas\ndasfdsafadsdfsa\nsdafsdafasdfda\nasdfasdfdasfddsfaasdf\nasdfsadfsadfsadf'),
-    new mealMenu('lunch', mkeMmt('11:30 a'), mkeMmt('1:30 p'), 'lunch\ndafsadfasdffsadfsadfd\nsadfdsafsadasdfdasfdasfdsafasdf\nsadfsadfasfsadasdfasdfadsfds\ndasgasdfdasasddasfdas\nasdfasdfasdfs'),
-    new mealMenu('dinner', mkeMmt('4:45 p'), mkeMmt('6:15 p'), 'dinner\nasdfsadfsdfasdfsdadsfadadf\nasdfasdfasfdsafdfdsa\nadsfasfdasddfsdafdsafdsaf\nasdfdasfsdafdsdadfsdafdadffsaddfsasdffdasdfdsfadsasdfadsfsad\ndfasdfa\nasdfads\nasdfasdfsad\nasedfsdfsd\nasdfsadfdsa\nasdfasdfdsa\nsadfasdfasddfasd\nasdfsadfdsa\nasdfsdafasdfasd\nasdfsadfasddfasd\nasdfasddfsaf\nasdfsaddfdsa\nasdfasdfsadfas\nasdfasdfasdfasdfdas\nasdfdasfdsadfsadfasdf'),
-]},
-    {"name": "Knollcrest",
-    "events": [
-    new mealMenu('breakfast', mkeMmt('8:30 a'), mkeMmt('10:00 a'), 'KNOOOOOOOOOOOOOOOOOOOOOOOOOLLLLL CREST!!!!!!!!'),
-    new mealMenu('lunch', mkeMmt('11:30 a'), mkeMmt('1:30 p'), 'KNOLLLW'),
-    new mealMenu('dinner', mkeMmt('4:45 p'), mkeMmt('6:15 p'), ':)'),
-]}];
+// Run makeVenue to test it
+makeVenues();
 
-// send today over
-app.get('/today', function (req, res) {
-    res.cacheControl({maxAge: "2h"});
-    res.send(todaysMenu);
-});
+function makeVenues () {
+    let venueCommons = makeVenue("Commons Dining Hall");
+    let venueCommons2 = makeVenue("Knollcrest Dining Hall");
+    
+}
 
-module.exports = app;
+function makeVenue (venueName) {
+    let requestDate = getDate();
+    console.log("Requesting date: " + requestDate);
+    let venueJSON = "https://calvin.edu/api/content/render/false/type/json/query/+structureName:DiningMenus%20+DiningMenus.venue:" + venueName + "%20+DiningMenus.date:" + requestDate + "%20+live:true/orderby/DiningMenus.startTime%20asc";
+    https.get(venueJSON, (res) => {
+        /*
+        console.log("statusCode:", res.statusCode);
+        console.log("headers:", res.headers);*/
+        
+        // https://nodejs.org/api/http.html#http_http_get_options_callback
+        // handle getting data
+        let vJson;
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => rawData += chunk);
+            res.on('end', () => {
+            try {
+                vJson = JSON.parse(rawData);
+                return makeVenueFromJSON(vJson, venueName);
+            } catch (e) {
+              console.log(e.message);
+            }
+        });
+        // end data handling
+    });
+}
+
+function makeVenueFromJSON (vJson, venueName) {
+    if (vJson.contentlets.length > 0) {
+        let newVenue = new Venue(venueName, null); 
+        for (var i = 0; i < vJson.contentlets.length; i++) {
+            let vData = vJson.contentlets;
+            let startTime = vData[i].startTime;
+            let endTime = vData[i].endTime;
+            let name = vData[i].mealName;
+            let description = vData[i].menuContent;
+            newVenue.addEvent(new mealMenu(name, startTime, endTime, description));
+        }
+        console.log("THE DATA IS:", newVenue);
+        return newVenue;
+    } else {
+        console.log("NO MENUES TODAY");
+    }
+}
+
+// Copied from calvin's server
+function getDate () {
+    var m_names = new Array("", "Jan.", "Feb.", "Mar.",
+        "Apr.", "May", "June", "July", "Aug.", "Sep.",
+        "Oct.", "Nov.", "Dec.");
+
+    // TODO use moment time zone
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1;
+    var curr_year = d.getFullYear();
+
+    /* Edit: 12/01/2015, jlaughli 
+       We need a two digit value for both date and month, 
+       otherwise the JSON request fails. */
+    var req_date = ("0" + d.getDate()).slice(-2);
+    var req_month = ("0" + (d.getMonth() + 1)).slice(-2);
+    var requestDate = curr_year + "" + req_month + "" + req_date;
+    return requestDate;
+}
