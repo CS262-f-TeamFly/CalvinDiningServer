a776f8bab841d15786300db858c32eec1bf69cc2
Krisofer Brink <kpgbrink@gmail.com>
2016-11-06 20:08:17 -0500

    Fixed errors and made JSON better

--- app.js
+++ app.js
@@ -21,12 +21,14 @@ function mkeMmt (time) {
     return moment.tz(time, "hh:mm a", "America/Detroit");
 }
 
-var todaysMenu = [{"Commons": [
+var todaysMenu = [{"name": "Commons",
+    "events": [
     new mealMenu('breakfast', mkeMmt('8:30 a'), mkeMmt('10:00 a'), 'contenental breakfast\nfdadffdas\ndasfdsafadsdfsa\nsdafsdafasdfda\nasdfasdfdasfddsfaasdf\nasdfsadfsadfsadf'),
     new mealMenu('lunch', mkeMmt('11:30 a'), mkeMmt('1:30 p'), 'lunch\ndafsadfasdffsadfsadfd\nsadfdsafsadasdfdasfdasfdsafasdf\nsadfsadfasfsadasdfasdfadsfds\ndasgasdfdasasddasfdas\nasdfasdfasdfs'),
     new mealMenu('dinner', mkeMmt('4:45 p'), mkeMmt('6:15 p'), 'dinner\nasdfsadfsdfasdfsdadsfadadf\nasdfasdfasfdsafdfdsa\nadsfasfdasddfsdafdsafdsaf\nasdfdasfsdafdsdadfsdafdadffsaddfsasdffdasdfdsfadsasdfadsfsad\ndfasdfa\nasdfads\nasdfasdfsad\nasedfsdfsd\nasdfsadfdsa\nasdfasdfdsa\nsadfasdfasddfasd\nasdfsadfdsa\nasdfsdafasdfasd\nasdfsadfasddfasd\nasdfasddfsaf\nasdfsaddfdsa\nasdfasdfsadfas\nasdfasdfasdfasdfdas\nasdfdasfdsadfsadfasdf'),
-]}
-    {"Knollcrest": [
+]},
+    {"name": "Knollcrest",
+    "events": [
     new mealMenu('breakfast', mkeMmt('8:30 a'), mkeMmt('10:00 a'), 'KNOOOOOOOOOOOOOOOOOOOOOOOOOLLLLL CREST!!!!!!!!'),
     new mealMenu('lunch', mkeMmt('11:30 a'), mkeMmt('1:30 p'), 'KNOLLLW'),
     new mealMenu('dinner', mkeMmt('4:45 p'), mkeMmt('6:15 p'), ':)'),
