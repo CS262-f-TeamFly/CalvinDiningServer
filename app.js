4663fa7211e47772533b8b8a385a58792e390d50
Krisofer Brink <kpgbrink@gmail.com>
2016-11-06 20:00:09 -0500

    Adding multiple venues

--- app.js
+++ app.js
@@ -21,11 +21,16 @@ function mkeMmt (time) {
     return moment.tz(time, "hh:mm a", "America/Detroit");
 }
 
-var todaysMenu = [
+var todaysMenu = [{"Commons": [
     new mealMenu('breakfast', mkeMmt('8:30 a'), mkeMmt('10:00 a'), 'contenental breakfast\nfdadffdas\ndasfdsafadsdfsa\nsdafsdafasdfda\nasdfasdfdasfddsfaasdf\nasdfsadfsadfsadf'),
     new mealMenu('lunch', mkeMmt('11:30 a'), mkeMmt('1:30 p'), 'lunch\ndafsadfasdffsadfsadfd\nsadfdsafsadasdfdasfdasfdsafasdf\nsadfsadfasfsadasdfasdfadsfds\ndasgasdfdasasddasfdas\nasdfasdfasdfs'),
     new mealMenu('dinner', mkeMmt('4:45 p'), mkeMmt('6:15 p'), 'dinner\nasdfsadfsdfasdfsdadsfadadf\nasdfasdfasfdsafdfdsa\nadsfasfdasddfsdafdsafdsaf\nasdfdasfsdafdsdadfsdafdadffsaddfsasdffdasdfdsfadsasdfadsfsad\ndfasdfa\nasdfads\nasdfasdfsad\nasedfsdfsd\nasdfsadfdsa\nasdfasdfdsa\nsadfasdfasddfasd\nasdfsadfdsa\nasdfsdafasdfasd\nasdfsadfasddfasd\nasdfasddfsaf\nasdfsaddfdsa\nasdfasdfsadfas\nasdfasdfasdfasdfdas\nasdfdasfdsadfsadfasdf'),
-];
+]}
+    {"Knollcrest": [
+    new mealMenu('breakfast', mkeMmt('8:30 a'), mkeMmt('10:00 a'), 'KNOOOOOOOOOOOOOOOOOOOOOOOOOLLLLL CREST!!!!!!!!'),
+    new mealMenu('lunch', mkeMmt('11:30 a'), mkeMmt('1:30 p'), 'KNOLLLW'),
+    new mealMenu('dinner', mkeMmt('4:45 p'), mkeMmt('6:15 p'), ':)'),
+]}];
 
 // send today over
 app.get('/today', function (req, res) {
