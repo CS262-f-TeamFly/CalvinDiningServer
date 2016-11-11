144bae8869ae5ec1dbf55c77badb607cf7d860f8
Krisofer Brink <kpgbrink@gmail.com>
2016-11-11 00:56:43 -0500

    add more menus

--- app.js
+++ app.js
@@ -58,6 +58,10 @@ function makeVenues () {
     return Promise.all([
         makeVenue("Commons Dining Hall"),
         makeVenue("Knollcrest Dining Hall"),
+        makeVenue("Johnnys Cafe"),
+        makeVenue("Spoelhof Cafe"),
+        makeVenue("Knight Way Cafe")
+        
     ]);
 }
 
